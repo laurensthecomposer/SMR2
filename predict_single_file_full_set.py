31833f6575377b382411082d9811930c21ec3e9b
Marc <dudley.marc@gmail.com>
2020-01-07 14:28:04 +0100

    less classes

--- predict_single_file_full_set.py
+++ predict_single_file_full_set.py
@@ -8,32 +8,28 @@ import sys
 
 #filepath = "Users\laure\Documents\SMR2\augmenting_image\1.jpg"
 
-filepath = "/Users/marcdudley/Downloads/SMR2/image_data_split/nas1802-3-6 copy/26.jpg"
+filepath = "/Users/marcdudley/Downloads/SMR2/green_tes/green_tes_test/ms9557-16/22.jpg"
 
 
 REV_CLASS_MAP = {
 
-    0: "m59557-10",
-    1: "m59557-16",
-    2: "m59557-20",
-    3: "nas1802-3-6",
-    4: "nas1802-3-7",
-    5: "nas1802-3-8",
-    6: "nas1802-3-9",
-    7: "nas1802-4-07",
-    8: "nas6305-10",
-    9: "none"
+    0: "m59557-16",
+    1: "m59557-20",
+    2: "nas1802-3-7",
+    3: "nas1802-3-9",
+    4: "nas6305-10",
+    5: "v647p23b"
 }
 
 def mapper(val):
     return REV_CLASS_MAP[val]
 
-model = load_model("full_set_val_acc_97_v1.h5")
+model = load_model("/Users/marcdudley/Downloads/SMR2/green_tes_v2_350px.h5")
 
 # prepare the image
 img = cv2.imread(filepath)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img = cv2.resize(img, (227, 227))
+img = cv2.resize(img, (350, 350))
 
 # predict the picture
 pred = model.predict(np.array([img]))
