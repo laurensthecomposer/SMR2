a221c72953904c7a5e25932bdd0efa9e44122d6f
Marc <dudley.marc@gmail.com>
2020-01-08 14:42:29 +0100

    split camera data into folders

--- predict_only_folder_class.py
+++ predict_only_folder_class.py
@@ -6,12 +6,17 @@ import os
 REV_CLASS_MAP = {
 
 
-    0: "m59557-16",
-    1: "m59557-20",
-    2: "nas1802-3-7",
-    3: "nas1802-3-9",
-    4: "nas6305-10",
-    5: "v647p23b",
+    0: "nas1802-3-6",
+    1: "nas1802-3-7",
+
+
+
+   # 0: "m59557-16",
+    #1: "m59557-20",
+    #2: "nas1802-3-7",
+    #3: "nas1802-3-9",
+    #4: "nas6305-10",
+    #5: "v647p23b",
 
 }
 
@@ -21,12 +26,11 @@ def mapper(val):
 model = load_model("/Users/marcdudley/Downloads/SMR2/green_tes_v2_350px.h5")
 
 # image folder
-folder_path = '/Users/marcdudley/Downloads/SMR2/green_tes/green_tes_test/nas1802-3-7'
+folder_path = '/Users/marcdudley/Downloads/SMR2/green_tes/test/nas1802-3-7'
 # path to model
 
 # dimensions of images
 img_width, img_height = 350, 350
-
 
 
 # load all images into a list
@@ -42,5 +46,5 @@ for img in os.listdir(folder_path):
 
 # stack up images list to pass for prediction
 images = np.vstack(images)
-classes = model.predict_classes(images, batch_size=1)
+classes = model.predict(images, batch_size=1)
 print(classes)
