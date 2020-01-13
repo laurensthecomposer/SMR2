58ccba3ce0f6f836fb2cde7f050a041656cfa47d
Laurens <laurensg100@gmail.com>
2020-01-13 11:12:54 +0100

    Auto stash before merge of "green_screen" and "origin/green_screen"

--- train_green_tes_set.py
+++ train_green_tes_set.py
@@ -11,33 +11,31 @@ from keras.preprocessing.image import ImageDataGenerator
 
 datagen = ImageDataGenerator()
 
-amount_train_images = 1296
 
-amount_validation_images = 373
+def generate_data(train_batch_size, validation_batch_size):
+    train_it = datagen.flow_from_directory( os.path.abspath( 'image_data_better_camera_more_split/train' ),
+                                            target_size=(350, 350), class_mode='categorical',
+                                            batch_size=train_batch_size )
 
-train_batch_size = 40
+    val_it = datagen.flow_from_directory( os.path.abspath( 'image_data_better_camera_more_split/validate' ),
+                                          target_size=(350, 350), class_mode='categorical',
+                                          batch_size=validation_batch_size )
 
-validation_batch_size = 20
+    amount_classes = len( os.listdir( os.path.abspath( 'image_data_better_camera_more_split/train' ) ) )
 
-train_it = datagen.flow_from_directory(os.path.abspath('image_data_better_camera_more_split/train'),target_size=(350,350), class_mode='categorical', batch_size=train_batch_size)
-
-val_it = datagen.flow_from_directory(os.path.abspath('image_data_better_camera_more_split/validate'),target_size=(350,350), class_mode='categorical', batch_size=validation_batch_size)
-
-amount_classes = len ( os.listdir(os.path.abspath('image_data_better_camera_more_split/train')) )
-
-
+    return train_it, val_it, amount_classes
 
 
-def get_model():
-    model = Sequential([
-        #change model res
-        SqueezeNet(input_shape=(350, 350, 3), include_top=False),
-        Dropout(0.5),
-        Convolution2D(amount_classes, (1, 1), padding='valid'),
-        Activation('relu'),
+def get_model(amount_classes):
+    model = Sequential( [
+        # change model res
+        SqueezeNet( input_shape=(350, 350, 3), include_top=False ),
+        Dropout( 0.5 ),
+        Convolution2D( amount_classes, (1, 1), padding='valid' ),
+        Activation( 'relu' ),
         GlobalAveragePooling2D(),
-        Activation('softmax')
-    ])
+        Activation( 'softmax' )
+    ] )
     return model
 
 model = get_model()
@@ -48,11 +46,31 @@ model.fit_generator(train_it, steps_per_epoch=(amount_train_images/train_batch_s
 
 model_name = "swek""
 
-name = os.path.join("ml_bolt_models", model_name)
 
-# save the model for later use
-model.save(name)
+    model = get_model( amount_classes )
+    model.compile( optimizer=Adam( lr=0.0001 ), loss='categorical_crossentropy', metrics=['acc'] )  # lr = learning rate
 
-#score = model.evaluate(np.array(data), np.array(labels))
+    model.fit_generator( train_it, steps_per_epoch=(amount_train_images / train_batch_size), validation_data=val_it,
+                         validation_steps=(amount_validation_images / validation_batch_size), epochs=epochs, verbose=1 )
 
-#print(score)
+    model_name = "green_tes_v3_640px.h5"
+    name = os.path.join("ml_bolt_models", model_name)
+    # save the model for later use
+    model.save(name)
+
+    #score = model.evaluate(np.array(data), np.array(labels))
+
+    name = ''.join( ["green_tes_v3_640px.h5"] )
+
+    # save the model for later use
+    model.save( name )
+
+
+# score = model.evaluate(np.array(data), np.array(labels))
+
+# print(score)
+if __name__ == "__main__":
+    train_batch_size = 40
+    validation_batch_size = 20
+    train_it, val_it, amount_classes = generate_data( train_batch_size, validation_batch_size )
+    make_model( train_it, val_it, amount_classes, train_batch_size, validation_batch_size )
