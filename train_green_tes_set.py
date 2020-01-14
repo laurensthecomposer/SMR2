501453eba54f865e9b6d1bde7747be7fbe0ad088
Laurens <laurensg100@gmail.com>
2020-01-14 11:31:21 +0100

    Recommendations from Guus

--- train_green_tes_set.py
+++ train_green_tes_set.py
@@ -9,7 +9,29 @@ import tensorflow as tf
 import os
 from keras.preprocessing.image import ImageDataGenerator
 
-datagen = ImageDataGenerator()
+datagen = ImageDataGenerator(featurewise_center=False,
+                             samplewise_center=False,
+                             featurewise_std_normalization=False,
+                             samplewise_std_normalization=False,
+                             zca_whitening=False,
+                             zca_epsilon=1e-06,
+                             rotation_range=0,
+                             width_shift_range=0.0,
+                             height_shift_range=0.0,
+                             brightness_range=None,
+                             shear_range=0.0,
+                             zoom_range=0.0,
+                             channel_shift_range=0.0,
+                             fill_mode='nearest',
+                             cval=0.0,
+                             horizontal_flip=False,
+                             vertical_flip=False,
+                             rescale=None,
+                             preprocessing_function=None,
+                             data_format='channels_last',
+                             validation_split=0.0,
+                             interpolation_order=1,
+                             dtype='float32')
 
 def generate_data(train_batch_size, validation_batch_size):
 
@@ -25,7 +47,6 @@ def get_model(amount_classes):
     model = Sequential([
         #change model res
         SqueezeNet(input_shape=(350, 350, 3), include_top=False),
-        Dropout(0.5),
         Convolution2D(amount_classes, (1, 1), padding='valid'),
         Activation('relu'),
         GlobalAveragePooling2D(),
