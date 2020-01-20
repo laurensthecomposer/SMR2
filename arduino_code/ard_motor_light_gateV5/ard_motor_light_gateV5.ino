fb08c58afc882e588f238a366085d581f435ba02
Laurens <laurensg100@gmail.com>
2020-01-20 13:56:25 +0100

    Arduino pwm to three individual pwm's. Added const values.

--- arduino_code/ard_motor_light_gateV5/ard_motor_light_gateV5.ino
+++ arduino_code/ard_motor_light_gateV5/ard_motor_light_gateV5.ino
@@ -6,29 +6,31 @@ int data = 0;
 int gatePin =12;
 int loaderPin = 11;
 int blockerPin = A1;
+
+const int pwm_m1 = 100;
+const int pwm_m2 = 200;
+const int pwm_m3 = 255;
+
 char pyInput;
-int pwm = 200;
 int blockerVal = 0;
 
 bool gateStatus; 
 
 // connect motor controller pins to Arduino digital pins
 // motor one
-int enA = 3; // pwm port 
-int in1 = 4;
-int in2 = 2;
-
-int pmwM1_diff = 100; 
+const int enA = 3; // pwm port 
+const int in1 = 4;
+const int in2 = 2;
 
 // motor two
-int enB = 5; // pwm port
-int in3 = 7;
-int in4 = 6;
+const int enB = 5; // pwm port
+const int in3 = 7;
+const int in4 = 6;  
 
 // motor three
-int enC = 10; // pwm port
-int in5 = 9;
-int in6 = 8;
+const int enC = 10; // pwm port
+const int in5 = 9;
+const int in6 = 8;
 
 int timer;
 
@@ -73,12 +75,12 @@ if(Serial.available()>0){
   pyInput = Serial.read();
   
 
-    if(pyInput == 'l' && gateStatus == LOW){
+    if(pyInput == 'l' && gateStatus == LOW){ // return the gate status
       data = 1;
       Serial.println(data);
 
     }
-    else if(pyInput == 'l' && gateStatus == HIGH){
+    else if(pyInput == 'l' && gateStatus == HIGH){ // return the gate status
       data = 2;
       Serial.println(data);
     }
@@ -87,7 +89,7 @@ if(Serial.available()>0){
       data = 3;
       digitalWrite(in1, HIGH);
       digitalWrite(in2, LOW);
-      analogWrite(enA, (pwm-pmwM1_diff));
+      analogWrite(enA, (pwm_m1));
       
       Serial.println(data);
     }
@@ -102,7 +104,7 @@ if(Serial.available()>0){
       data = 5;     
       digitalWrite(in1, LOW);
       digitalWrite(in2, HIGH); 
-      analogWrite(enA, pwm);
+      analogWrite(enA, pwm_m1);
       
       Serial.println(data);
     }
@@ -111,7 +113,7 @@ if(Serial.available()>0){
       data = 6;
       digitalWrite(in3, HIGH);
       digitalWrite(in4, LOW);
-      analogWrite(enB, pwm);
+      analogWrite(enB, pwm_m2);
       
       Serial.println(data);
     }
@@ -126,7 +128,7 @@ if(Serial.available()>0){
       data = 8;     
       digitalWrite(in3, LOW);
       digitalWrite(in4, HIGH); 
-      analogWrite(enB, pwm);
+      analogWrite(enB, pwm_m2);
       
       Serial.println(data);
     }
@@ -135,7 +137,7 @@ if(Serial.available()>0){
       data = 9;
       digitalWrite(in5, HIGH);
       digitalWrite(in6, LOW);
-      analogWrite(enC, pwm-75);
+      analogWrite(enC, pwm_m3);
       
       Serial.println(data);
     }
@@ -150,7 +152,7 @@ if(Serial.available()>0){
       data = 11;     
       digitalWrite(in5, LOW);
       digitalWrite(in6, HIGH); 
-      analogWrite(enC, pwm);
+      analogWrite(enC, pwm_m3);
       
       Serial.println(data);
       
@@ -174,13 +176,13 @@ if(Serial.available()>0){
       data = 13;     
       digitalWrite(in1, HIGH);
       digitalWrite(in2, LOW); 
-      analogWrite(enA, (pwm-pmwM1_diff));
+      analogWrite(enA, pwm_m1);
       digitalWrite(in3, HIGH);
       digitalWrite(in4, LOW); 
-      analogWrite(enB, pwm);
+      analogWrite(enB, pwm_m2);
       digitalWrite(in5, HIGH);
       digitalWrite(in6, LOW); 
-      analogWrite(enC, pwm);
+      analogWrite(enC, pwm_m3);
       
       Serial.println(data);
     }
