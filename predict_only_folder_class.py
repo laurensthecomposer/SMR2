6b7c61609d1a3228bd146777f19c56121644dc5e
Laurens <laurensg100@gmail.com>
2020-01-22 11:05:45 +0100

    predict on model

--- predict_only_folder_class.py
+++ predict_only_folder_class.py
@@ -1,5 +1,7 @@
-from keras.models import load_model
-from keras.preprocessing import image
+
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 import os
 
@@ -23,28 +25,28 @@ REV_CLASS_MAP = {
 def mapper(val):
     return REV_CLASS_MAP[val]
 
-model = load_model("/Users/marcdudley/Downloads/SMR2/green_tes_v2_350px.h5")
+model = load_model("models/image_data_blue_light_split_sq350_e120_tb40_vb40_aug-hor-rotran20-briran0_8;1_2).h5")
+model.summary()
 
 # image folder
-folder_path = '/Users/marcdudley/Downloads/SMR2/green_tes/test/nas1802-3-7'
-# path to model
+folder_path = 'dataset/image_data_blue_light_split/test/'
+roi_square_size = 350
+test_batch_size = 10
 
-# dimensions of images
-img_width, img_height = 350, 350
+test_datagen = ImageDataGenerator()
 
+test_it = test_datagen.flow_from_directory(
+    os.path.abspath(folder_path ),
+    target_size=(roi_square_size, roi_square_size),
+    class_mode='categorical',
+    batch_size=test_batch_size
+)
 
-# load all images into a list
-images = []
-for img in os.listdir(folder_path):
-    if img.startswith("."):
-        continue
-    img = os.path.join(folder_path, img)
-    img = image.load_img(img, target_size=(img_width, img_height))
-    img = image.img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    images.append(img)
 
 # stack up images list to pass for prediction
-images = np.vstack(images)
-classes = model.predict(images, batch_size=1)
-print(classes)
+# classes = model.predict(images, batch_size=1)
+# loss, acc = model.evaluate(test_it, verbose=2)
+prediction = model.predict(test_it, verbose=1)
+type(prediction)
+print(prediction)
+# print(classes)
