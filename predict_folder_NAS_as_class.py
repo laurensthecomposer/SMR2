0a46c410dd1c9da11a573b039cab09ee52c09ffb
Marc <dudley.marc@gmail.com>
2020-01-22 12:57:18 +0100

    tensorflow2 update

--- predict_folder_NAS_as_class.py
+++ predict_folder_NAS_as_class.py
@@ -1,31 +1,40 @@
-from keras.models import load_model
-from keras.preprocessing import image
+import tensorflow
+print(tensorflow.__version__)
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 import os
 
 REV_CLASS_MAP = {
 
 
-    0: "nas1802-3-6",
-    1: "nas1802-3-7",
-    2: "nas1802-3-8",
-    3: "nas1802-3-9",
+
+    0: "m59557-10",
+    1: "m59557-16",
+    2: "m59557-20",
+    3: "nas1802-3-6",
+    4: "nas1802-3-7",
+    5: "nas1802-3-8",
+    6: "nas1802-3-9",
+    7: "nas1802-4-07",
+    8: "v647p23b"
 
 }
 
 def mapper(val):
     return REV_CLASS_MAP[val]
-model_path = "models/bc_NAS_only_v1_750pix.h5"
+model_path = "/Users/marcdudley/Downloads/SMR2/models/image_data_blue_light_split_sq600_e160_tb60_vb60_aug-hor-rotran20-briran0_8;1_2).h5"
 model_path = os.path.abspath(model_path)
 model = load_model(model_path)
 
 # image folder
-folder_path = 'dataset/old_image_data/image_data_bc_nas_only/test/nas1802-3-9'
+folder_path = 'dataset/image_data_blue_light_split/test/m59557-10'
 folder_path = os.path.abspath(folder_path)
 # path to model
 
 # dimensions of images
-img_width, img_height = 550, 550
+img_width, img_height = 600, 600
 
 
 
@@ -42,12 +51,12 @@ for img in os.listdir(folder_path):
 images = np.vstack(images)
 classes = model.predict(images, batch_size=1)
 
-classes2 = model.predict_classes(images, batch_size=1)
+#classes2 = model.predict(images, batch_size=1)
 
 np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
 
 print(classes)
 
-print(classes2)
+
 
 
--- predict_only_folder_class.py
+++ predict_only_folder_class.py
@@ -9,25 +9,24 @@ import os
 REV_CLASS_MAP = {
 
 
-    0: "nas1802-3-6",
-    1: "nas1802-3-7",
 
-
-
-   # 0: "m59557-16",
-    #1: "m59557-20",
-    #2: "nas1802-3-7",
-    #3: "nas1802-3-9",
-    #4: "nas6305-10",
-    #5: "v647p23b",
+    0: "m59557-10",
+    1: "m59557-16",
+    2: "m59557-20",
+    3: "nas1802-3-6",
+    4: "nas1802-3-7",
+    5: "nas1802-3-8",
+    6: "nas1802-3-9",
+    7: "nas1802-4-07",
+    8: "v647p23b"
 
 }
 
 def mapper(val):
     return REV_CLASS_MAP[val]
 
-# model = load_model(os.path.abspath("models/image_data_blue_light_split_sq350_e120_tb40_vb40_aug-hor-rotran20-briran0_8;1_2.h5"))
-model = load_model("/Users/marcdudley/Downloads/SMR2/models/image_data_blue_light_split_sq350_e120_tb40_vb40_aug-hor-rotran20-briran0_8;1_2.h5")
+model = load_model(os.path.abspath("models/image_data_blue_light_split_sq600_e160_tb60_vb60_aug-hor-rotran20-briran0_8;1_2).h5"))
+
 # model.summary()
 
 # image folder
@@ -44,10 +43,10 @@ test_it = test_datagen.flow_from_directory(
     batch_size=test_batch_size
 )
 
-
+np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
 # stack up images list to pass for prediction
 # classes = model.predict(images, batch_size=1)
-# loss, acc = model.evaluate(test_it, verbose=2)
+#loss, acc = model.evaluate(test_it, verbose=1)
 prediction = model.predict(test_it, verbose=1)
 type(prediction)
 print(prediction)
