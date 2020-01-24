8514c4e91c7dd8b275a4157dcb44ee0c75730a14
Marc <dudley.marc@gmail.com>
2020-01-24 10:05:48 +0100

    testing files

--- predict_only_folder_class.py
+++ predict_only_folder_class.py
@@ -6,7 +6,11 @@ import os
 REV_CLASS_MAP = {
 
 
-# models/blue_light_crop_triple_sq550_e560_tb20_vb20_aug.h5, tested on 378 images 99.2pc correct
+# models/blue_light_crop_triple_sq550_e560_tb20_vb20_aug.h5,
+    # tested on 378 images 99.2pc correct
+
+    # Description: image is cropped into a rectangle close to the bolt and made into a square
+    # with the remaining space filled with 2 duplicate rectangles (of the bolt)
 
   #  0: "m59557-10", # all correct
    # 1: "m59557-16", # all correct
@@ -19,28 +23,66 @@ REV_CLASS_MAP = {
     #8: "nas6305-10", # all correct
     #9: "v647p23b" # all correct
 
+#models/blue_light_crop_black_background_sq550_e560_tb20_vb20_aug-hor-briran0_8;1_2.h5
+    # tested on 378 images 99.47pc correct
+
+    # Description: image is cropped into a rectangle close to the bolt and made into a square
+    # the remaining space masked black
+
+   # 0: "m59557-10", # all correct
+   # 1: "m59557-16", # all correct
+   # 2: "m59557-20", # 1 HIGH false-pos
+   # 3: "nas1802-3-6", # all correct
+   # 4: "nas1802-3-7", # all correct
+   # 5: "nas1802-3-8", # 1 low false positive
+   # 6: "nas1802-3-9", # all correct
+   # 7: "nas1802-4-07", # all correct
+   # 8: "nas6305-10", # all correct
+   # 9: "v647p23b" # all correct
+
+#models/blue_light_crop_split_beneath_sq550_e560_tb20_vb20_aug-briran0_8;1_2.h5
+    # tested on 378 images 99.73pc correct
+
+    # Description: image is cropped halfway through the bolt and added underneath ALL BELT STRIPS REMOVED
 
 
-    0: "m59557-10", # all correct
-    1: "m59557-16", # all correct
-    2: "m59557-20", # all correct
-    3: "nas1802-3-6", # all correct
-    4: "nas1802-3-7", # (1 incorrect with very LOW false-pos)
-    5: "nas1802-3-8", # (2 incorrect MED and HIGH false-pos)
-    6: "nas1802-3-9", # all correct
-    7: "nas1802-4-07", # all correct
-    8: "nas6305-10", # all correct
-    9: "v647p23b" # all correct
+   # 0: "m59557-10", # all correct
+   # 1: "m59557-16", # all correct
+   # 2: "m59557-20", # all correct
+   # 3: "nas1802-3-6", # all correct
+   # 4: "nas1802-3-7", # all correct
+   # 5: "nas1802-3-8", # 1 high false positive
+   # 6: "nas1802-3-9", # all correct
+   # 7: "nas1802-4-07", # all correct
+   # 8: "nas6305-10", # all correct
+   # 9: "v647p23b" # all correct
+
+#models/blue_light_split2_sq550_e520_tb20_vb20_aug-hor-briran0_8;1_2.h5
+    # tested on 378 images 99.73pc correct
+
+    # Description: image is raw and resized to 550px sq
+
+
+   # 0: "m59557-10", # all correct
+   # 1: "m59557-16", # all correct
+   # 2: "m59557-20", # all correct
+   # 3: "nas1802-3-6", # all correct
+   # 4: "nas1802-3-7", # all correct
+   # 5: "nas1802-3-8", # 1 high false positive
+   # 6: "nas1802-3-9", # all correct
+   # 7: "nas1802-4-07", # all correct
+   # 8: "nas6305-10", # all correct
+   # 9: "v647p23b" # all correct
 
 }
 
 def mapper(val):
     return REV_CLASS_MAP[val]
 
-model = load_model("models/blue_light_split_tf1_sq550_e500_tb20_vb20_aug-hor-briran0_8;1_2.h5")
+model = load_model("models/blue_light_split2_sq550_e520_tb20_vb20_aug-hor-briran0_8;1_2.h5")
 
 # image folder
-folder_path = 'dataset/image_data_blue_light_more/nas1802-3-6'
+folder_path = 'dataset/image_data_blue_light_split/test/v647p23b'
 # path to model
 
 # dimensions of images
@@ -62,6 +104,6 @@ np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
 # stack up images list to pass for prediction
 images = np.vstack(images)
 
-classes = model.predict_classes(images, batch_size=3)
+classes = model.predict(images, batch_size=3)
 print(classes)
 
