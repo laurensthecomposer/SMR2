397568b74edd5f6929de6333c70b2d5d0c0c85b5
Laurens <laurensg100@gmail.com>
2020-01-27 11:25:19 +0100

    added previous next functionality

---
+++ main_next_prev_button.py
@@ -0,0 +1,34 @@
+from ui_main_window import Ui_MainWindow
+from PySide2.QtWidgets import *
+from PySide2.QtCore import *
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.stackedWidget.setCurrentIndex(0)
+
+        self.ui.next_button.setEnabled(True)
+        self.ui.next_button.clicked.connect(self.goToNext)
+        self.ui.prev_button.setEnabled(True)
+        self.ui.prev_button.clicked.connect(self.goToPrev)
+        print(self.ui.stackedWidget.)
+
+    def goToNext(self):
+        index = self.ui.stackedWidget.currentIndex()
+        self.ui.stackedWidget.setCurrentIndex(index + 1)
+
+    def goToPrev(self):
+        index = self.ui.stackedWidget.currentIndex()
+        self.ui.stackedWidget.setCurrentIndex(index - 1)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
