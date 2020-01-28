e8e37cb1de39e1f0270a6076935637c0489caebd
Marcel <Marcel.koop@student.hu.nl>
2020-01-28 13:31:20 +0100

    test file for machine controller

--- machine_controller_test.py
+++ machine_controller_test.py
@@ -23,44 +23,47 @@ class MachineController(object):
         self.model_img_size = (550, 550)
 
     def connect_arduino(self):
+        print("connected arduino")
         return True
 
     def connect_camera(self):
+        print("connected camera")
         return True
 
     def connect_robot(self):
+        print("connected robot")
         return True
 
     def data_startup(self):
         self.count, _, self.IMG_CLASS_PATH = self.machine.save_pictures(self.IMG_SAVE_PATH, self.bolt_type_path, 0)
         self.REV_CLASS_MAP, self.file_path = self.data.bolts_in_model(sub_ass=1)
         self.model = load_model(self.file_path)
+        print("Loaded model")
 
     def machine_startup(self):
-        print("")
-        self.rob.startup(self.arduino)
-        self.arduino.blocker_close()
+        print("rob.startup(arduino)")
+        print("arduino.blocker_close()")
         time.sleep(0.5)
 
     def belts_roll(self):
-        self.arduino.all_forward()
-        self.arduino.bulk_feeder_start()
+        print("arduino.all_forward() / arduino.bulk_feeder_start()")
 
     def lightgate(self):
-        return self.arduino.gate_state
+        time.sleep(1)
+        print("bolt found")
+        return True
 
     def img_stop(self):
-        self.arduino.bulk_feeder_stop()
-        self.arduino.all_stop()
+        print("arduino.bulk_feeder_stop() / arduino.all_stop()")
         time.sleep(2)
 
     def img_capture(self):
-        ret, frame = self.camera.read()
-        ret, frame = self.camera.read()
-        return ret, self.frame
+        self.frame = cv2.imread("dataset/image_data_blue_light_more/nas1802-3-7/16.jpg")
+        return self.frame
 
     def img_processing(self, frame):
         crop_img = self.crop(frame, self.crop_dim['pos_x'], self.crop_dim['pos_y'], self.crop_dim['width'], self.crop_dim['height'])
+        print("Image Cropped")
         return crop_img
 
     def crop(self, img, x=100, y=200, width=600, height=600, ):
@@ -70,6 +73,7 @@ class MachineController(object):
         filename = '{}.jpg'.format(self.count + 1)
         save_path = os.path.join(self.IMG_CLASS_PATH, filename)
         cv2.imwrite(save_path, frame)
+        print("Image Saved")
 
     def img_classify(self, frame):
         bolt_type, pred, bolt_code = self.machine.test_img(frame, self.model, self.REV_CLASS_MAP, size=self.model_img_size)
@@ -77,21 +81,13 @@ class MachineController(object):
         return bolt_type, pred, bolt_code
 
     def exit_classified_bolt(self):
-        self.arduino.blocker_open()
-        time.sleep(0.5)
-        self.arduino.all_forward()
-        time.sleep(0.4)  # wait until object is gone (don't go lower)
-        self.arduino.blocker_close()  # close gate
-        time.sleep(0.4)  # bolt goes into robot/output
-        self.arduino.all_stop()
+        print("Bolt thrown out")
 
     def robot_drop(self, bolt_type):
-        self.rob.drop(self.arduino, bolt_type)
         print("Dropped: ", bolt_type)
 
     def machine_stop(self):
-        self.arduino.bulk_feeder_stop()
-        self.arduino.all_stop()
+        print("Machine stopped")
 
 
 
